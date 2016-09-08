df8efbda7cd1577f720c663d3cd7ffcb389f7111
Tommy Queenan <tommy.queenan@gmail.com>
2016-09-08 14:34:16 -0600

    Ste up all SFML

--- Chrono Split/Chrono Split/C++/Main.cpp
+++ Chrono Split/Chrono Split/C++/Main.cpp
@@ -8,21 +8,18 @@
 //Include Their Files
 #include <iostream> //input output
 #include <chrono> //high-res timer (chrono::high_resolution_clock::timepoint & chrono::high_resolution_clock::now())
+#include <SFML\Window.hpp> //Gui elements
 
 //Include Our files
 #include "../C++/Sprites/sprite.cpp" //import sprite.cpp
 #include "../C++/Render/render.cpp" //import render.cpp
 #include "../C++/Physics/physics.cpp" //import physics.cpp
 
-#include <SFML/Window.hpp>
-
 //Using
 using namespace std; //for easy cout & cin
 
 int main()
 {
-	sf::Window screen(sf::VideoMode(800, 600), "Chrono Split");
-
 	cout << "Hello world" << endl;
 	system("pause");
 	return 0; //exited corectly
