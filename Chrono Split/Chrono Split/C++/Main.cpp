ffc8ef4e878456cf0a59a5c5e0eb30f56f3c146b
Tommy Queenan <tommy.queenan@gmail.com>
2016-09-08 19:35:11 -0600

    Added a line

--- Chrono Split/Chrono Split/C++/Main.cpp
+++ Chrono Split/Chrono Split/C++/Main.cpp
@@ -26,6 +26,7 @@ int main()
     
     // Creates a new window object stored in the "window" variable
     sf::RenderWindow window(sf::VideoMode(800, 600), "Chrono Split");
+
 	// Limit the FPS to 60
 	window.setFramerateLimit(60);
 
