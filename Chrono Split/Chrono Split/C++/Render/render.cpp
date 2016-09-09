319b3c32cd9278e3ad55b1fc530eb6e8182bc5c3
Remington Thurber <remingtonthurber@gmail.com>
2016-09-08 20:34:49 -0600

    Added more draw functions to render_engine

--- Chrono Split/Chrono Split/C++/Render/render.cpp
+++ Chrono Split/Chrono Split/C++/Render/render.cpp
@@ -1,5 +1,7 @@
 #include <SFML/Graphics.hpp>
-#include <SFML/Graphics/Color.hpp>
+
+// Get the type of our sprites
+#include "../Sprites/sprite.cpp"
 
 #include <iostream>
 
@@ -18,17 +20,39 @@ namespace Render {
             shape.setFillColor(sf::Color::Green);
         }
         
-        // Draws a green circle to the screen every frame
-        int draw(sf::RenderWindow *window)
+        int clear(sf::RenderWindow *window)
         {
-            // Clear the screen to black
             window->clear(sf::Color::Black);
             
-            // Draws a green circle
-            window->draw(shape);
-            
-            // Pushes the draw updates to the display (so they actually show up)
+            return 0;
+        }
+        
+        int display(sf::RenderWindow *window)
+        {
             window->display();
+        }
+        
+        // Draws a green circle to the screen every frame
+        int draw(sf::RenderWindow *window, Sprites::sprite sprite)
+        {
+            window->draw(sprite);
+            
+            return 0;
+        }
+        
+        int draw_list(sf::RenderWindow *window, Sprites::sprite sprites[])
+        {
+            for (unsigned int i = 0; i < sizeof(sprites) / sizeof(sprites[0]); i++)
+            {
+                window->draw(sprites[i]);
+            }
+            
+            return 0;
+        }
+        
+        int draw_green_circle(sf::RenderWindow *window)
+        {
+            window->draw(shape);
             
             return 0;
         }
