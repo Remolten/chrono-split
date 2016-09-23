0bb0724912214c26d62d1fe58ce74d18e469f1a9
Remington Thurber <remingtonthurber@gmail.com>
2016-09-23 13:45:07 -0600

    Fixed logic error

--- Chrono Split/Chrono Split/C++/Physics/physics.cpp
+++ Chrono Split/Chrono Split/C++/Physics/physics.cpp
@@ -13,11 +13,9 @@ namespace physics
                 entity2.topLeft.y <= entity1.topLeft.y + entity2.size.y)
             {
                 // Reverse the objects velocity if it is moving at all
-                if (!(entity1.velocity.x == 0) && !(entity1.velocity.y == 0))
-                {
-                    entity1.velocity.x *= -1;
-                    entity1.velocity.y *= -1;
-                }
+                entity1.velocity.x *= -1;
+                entity1.velocity.y *= -1;
+                
                 // Just move the entity so it doesn't get stuck
                 // Change the position so the object is not colliding with whatever anymore
                 sf::Vector2f newPosition = sf::Vector2f(entity1.topLeft.x + entity1.velocity.x, entity1.topLeft.y + entity1.velocity.y);
